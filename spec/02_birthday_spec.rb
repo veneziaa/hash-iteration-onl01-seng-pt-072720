8931b1f340abe72245b7d54ab8777ff66697137b
Venice Vargas <venicevargas1@live.com>
2020-08-09 23:44:00 +0000

    Done.

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -1,29 +1,14 @@
 require_relative "../lib/birthday.rb"
 
-describe "#happy_birthday" do
-  it "prints out a birthday message to each kid in the birthday_kids hash" do
+describe "#happy_birthday" do 
+  it "prints out a birthday message to each kid in the birthday_kids hash" do 
     birthday_kids = {
-      "Timmy" => 9,
-      "Sarah" => 6,
+      "Timmy" => 9, 
+      "Sarah" => 6, 
       "Amanda" => 27
     }
 
     expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
-
-  end
-end
-
-## BONUS ##
-## Change "xit" to "it" in the test block to enable
-describe "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" do
-    birthday_kids = {
-      "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27
-    }
-
-    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
   end
 end
